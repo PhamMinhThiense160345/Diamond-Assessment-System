7e86661f8aaa17b1363c1d9a0a87d520c95fe830
clone1Rick <namtbse170411@fpt.edu.vn>
2024-06-04 21:34:28 +0700

    oke

--- DiamondAssesment/src/main/java/com/swp/DiamondAssesment/model/AssessmentRequests.java
+++ DiamondAssesment/src/main/java/com/swp/DiamondAssesment/model/AssessmentRequests.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Builder
 public class AssessmentRequests {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private int totalAmount;
