5fffee0388cf1d8a25335f1b76a3c26cd3c14e88
clone1Rick <namtbse170411@fpt.edu.vn>
2024-06-13 23:54:13 +0700

    inspect

--- DiamondAssesment/src/main/java/com/swp/DiamondAssesment/service/assessmentRequestService.java
+++ DiamondAssesment/src/main/java/com/swp/DiamondAssesment/service/assessmentRequestService.java
@@ -2,10 +2,12 @@ package com.swp.DiamondAssesment.service;
 
 import com.swp.DiamondAssesment.DTO.ResponseObject;
 import com.swp.DiamondAssesment.DTO.assessmentRequestDTO;
+import com.swp.DiamondAssesment.DTO.inspectParameterDTO;
 import com.swp.DiamondAssesment.DTO.searchRequestDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface assessmentRequestService {
+
     ResponseEntity<ResponseObject> createRequest(assessmentRequestDTO assessmentRequestDTO);
 
     ResponseEntity<ResponseObject> acceptRequest(int assessmentRequestDetailID);
@@ -16,4 +18,6 @@ public interface assessmentRequestService {
     ResponseEntity<ResponseObject> receiveDelegation(int assessmentRequestDetailID, int managerID);
 
     ResponseEntity<ResponseObject> searchRequest(searchRequestDTO searchDTO);
+
+    ResponseEntity<ResponseObject> saveAssessment(inspectParameterDTO inspectParameterDTO);
 }
